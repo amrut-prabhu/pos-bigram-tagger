05e9daf479708e2a59e822306640b2f683caba27
Amrut Prabhu <amrut08@gmail.com>
2019-10-10 23:15:34 +0800

    Format runtagger (accuracy 95.63% and 88.45%)

--- runtagger.py
+++ runtagger.py
@@ -6,26 +6,33 @@ import sys
 import datetime
 import numpy as np
 
+from collections import defaultdict
 import _pickle as pickle
+
+MIN_TRANSITION = -99999999
 
 # Markers for start and end of sentence
 START_MARKER = '<s>'
 END_MARKER = '</s>'
 
+tag_freq = defaultdict(int)
+bitag_freq = {}
+
+tags = {}
+num_tokens = {}
+tags_for_word = {}
+
 transition = {} 
 emission = {}
-tags_for_word = {}
-tag_freq = {}
-tags = {}
 
 transition_backoff = {}
 emission_backoff = {}
+
 transition_singleton = {}
 emission_singleton = {}
+
 transition_smoothed = {}
 emission_smoothed = {}
-bitag_freq = {}
-num_tokens = {}
 
 def tag_sentence(test_file, model_file, out_file):
     load_model(model_file)
@@ -40,57 +47,17 @@ def tag_sentence(test_file, model_file, out_file):
         cur_line = test_lines[i].strip()
         cur_words = cur_line.split(' ')
 
-        tagged_sentence = run_viterbi(cur_words, transition, emission)
+        tagged_sentence = run_viterbi(cur_words)
         output_writer.write(tagged_sentence + '\n')
     
     output_writer.close()
 
     print('Finished...')
 
-def get_tag_freq(u):
-    if u in tag_freq:
-        return tag_freq[u]
-    return 0
-
-def get_tags(word):
-    global tags
-    if word in tags_for_word:
-        return list(tags_for_word[word])
-    return list(tags)
-
-def get_emission_backoff(word):
-    if word in emission_backoff:
-        return emission_backoff[word]
-
-    V = len(tags)
-    return float(1) / float(num_tokens + V)
-
-def get_smoothed_emission(word, tag):
-    if (word, tag) in emission_smoothed:
-        return emission_smoothed[(word, tag)]
-    else:
-        lamda = 1 + emission_singleton.get(tag, 0)
-        return float(lamda * get_emission_backoff(word)) / float(tag_freq[tag] + lamda) # math.log(
-
-def get_smoothed_transition(tag_j, tag_i):
-    if (tag_j, tag_i) in transition_smoothed:
-        """ transition probability P(tag_i | tag_i - 1) """
-        transition_probability = transition_smoothed[(tag_j, tag_i)]
-    else:
-        """ Smoothed transition probability """
-        tag_freq = get_tag_freq(tag_j)
-        lamda = 1 + transition_singleton.get(tag_j, 0)
-        transition_probability = float(lamda * transition_backoff[tag_i]) / float(tag_freq + lamda) # math.log(
-    return transition_probability
-
-
 """
 obs: sequence of words in the sentence
-states: POS tags
-transition: transition probabilities
-emission: emission probabilities
 """
-def run_viterbi(obs, transition, emission):
+def run_viterbi(obs):
     """
     Viterbi probabilities: v(s, t)
     Maximum probability of all paths ending in state s_j at time t
@@ -98,26 +65,22 @@ def run_viterbi(obs, transition, emission):
     """
     viterbi = []
 
-    global tags
-        
     for t in range(0, len(obs)):
         viterbi.append({})
 
         # TODO: change this and the next loop to int
-        for st in get_tags(obs[t]): 
-            """ Base case needs to be handled here """
+        for st in get_tags_for_word(obs[t]): 
             if t == 0:
-                """ # calculate score using P(Ci|'^', '^') and P(Wj|Ci) """
                 viterbi[t][st] = {
                     "prob": get_smoothed_transition(START_MARKER, st) * get_smoothed_emission(obs[t], st), # +
                     "back_ptr": None
                 }
             else:
                 # Find state that maximises transition probability from the previous state
-                max_transition_prob = -100000
+                max_transition_prob = MIN_TRANSITION
                 back_ptr = None
 
-                for prev_state in get_tags(obs[t - 1]):
+                for prev_state in get_tags_for_word(obs[t - 1]):
                     transition_prob = viterbi[t - 1][prev_state]["prob"] * get_smoothed_transition(prev_state, st) # +
 
                     if transition_prob > max_transition_prob:
@@ -133,11 +96,11 @@ def run_viterbi(obs, transition, emission):
     viterbi_tags = []
 
     # Get the most probable final state and its backtrack
-    max_transition_prob = -100000
+    max_transition_prob = MIN_TRANSITION
     back_ptr = None
 
-    states = get_tags(obs[-1])
-    for prev_state in states:
+    # TODO: extract method
+    for prev_state in get_tags_for_word(obs[-1]):
         transition_prob = viterbi[-1][prev_state]["prob"] * get_smoothed_transition(prev_state, END_MARKER)
 
         if transition_prob > max_transition_prob:
@@ -152,10 +115,35 @@ def run_viterbi(obs, transition, emission):
         viterbi_tags.insert(0, viterbi[t][current]["back_ptr"])
         current = viterbi[t][current]["back_ptr"]
 
-    # TODO: take log of probabilities, when generating them in buildtagger
-    # Instead of P1 * P2, do log(P1) + log(P2)
-
     return ' '.join([word + '/' + tag for word, tag in zip(obs, viterbi_tags)])
+
+def get_tags_for_word(word):
+    if word in tags_for_word:
+        return list(tags_for_word[word])
+    return list(tags)
+
+def get_emission_backoff(word):
+    if word in emission_backoff:
+        return emission_backoff[word]
+
+    num_tags = len(tags)
+    return float(1) / float(num_tokens + num_tags)
+
+def get_smoothed_emission(word, tag):
+    if (word, tag) in emission_smoothed:
+        return emission_smoothed[(word, tag)]
+    else:
+        lamda = 1 + emission_singleton.get(tag, 0)
+        return float(lamda * get_emission_backoff(word)) / float(tag_freq[tag] + lamda) # math.log(
+
+def get_smoothed_transition(prev_tag, tag):
+    if (prev_tag, tag) in transition_smoothed:
+        transition_probability = transition_smoothed[(prev_tag, tag)]
+    else:
+        lamda = 1 + transition_singleton.get(prev_tag, 0)
+        transition_probability = float(lamda * transition_backoff[tag]) / float(tag_freq[prev_tag] + lamda) # math.log(
+    
+    return transition_probability
 
 def load_model(model_file):
     global tag_freq, bitag_freq
